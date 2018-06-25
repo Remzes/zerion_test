84280e923715eeb56f33088ee9514690f69ce834
Viktor Salnichenko <victor_s1998@mail.ru>
2018-06-25 05:54:49 +0300

    Update task_2.js

--- tasks/task_2.js
+++ tasks/task_2.js
@@ -1,3 +1,6 @@
+//CommentListInstance - это класс (className) компонента, на который был использован
+//данный HOC, он же декоратор. Почему я так делаю, я прописал в сообщении на моем круге
+
 import React, {Component} from 'react'
 import {lifecycle, compose} from 'recompose';
 
